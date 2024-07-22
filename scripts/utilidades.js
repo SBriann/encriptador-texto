04edcf943278ec7df4ef37d12d79a3f1dc9c7c88
Brian <briann.sandovalm@gmail.com>
2024-07-22 18:59:06 -0500

    Creación función validar texto

---
+++ scripts/utilidades.js
@@ -0,0 +1,17 @@
+// Validar que el texto cumpla con las restricciones (NO mayusculas - NO caracteres especiales)
+function validarTexto(texto) {
+    let esValido = true; // Valor a retornar
+    let listaCaracteres = texto.split(' '); // Lista caracteres (sin espacios) 
+
+    // Ciclo para recorrer la lista de caracteres
+    for (let caracter of listaCaracteres) {
+        // Validacion de cada caracter usando Unicode en expresion regular
+        if (!/^[\u0061-\u007A\u0030-\u0039]+$/.test(caracter)){
+            // \u0061-\u007A = a-z
+            // \u0030-\u0039 = 0-9
+            esValido = false; // El texto no es valido
+            break;
+        }
+    }
+    return esValido;
+}
