13f8b51464fb2891663fe713f363f4eccabd36df
Brian <briann.sandovalm@gmail.com>
2024-07-23 22:49:29 -0500

    Implementación validar texto en desencriptar

--- scripts/desencriptador.js
+++ scripts/desencriptador.js
@@ -9,15 +9,22 @@ let desencriptacion = {
 function desencriptarTexto () {
     let mensajeRecibido = document.getElementById('areaTexto').value; // Valor recibido
     let mensajeDesencriptado = ""; // Almacena mensaje desencriptado
+    let esValido = validarTexto(mensajeRecibido); // Validacion mayusculas y caracteres especiales
 
-    mensajeDesencriptado = mensajeRecibido;
-    
-    // Ciclo para evaluar el valor de cada llave de desencriptacion
-    for (let llave in desencriptacion) {
-        // Expresion regular que indica que se reemplace todas las coincidencias
-        let expRegular = new RegExp(llave, 'g');
-        mensajeDesencriptado = mensajeDesencriptado.replace(expRegular, desencriptacion[llave]);
+    if (esValido) {
+        mensajeDesencriptado = mensajeRecibido;
+        
+        // Ciclo para evaluar el valor de cada llave de desencriptacion
+        for (let llave in desencriptacion) {
+            // Expresion regular que indica que se reemplace todas las coincidencias
+            let expRegular = new RegExp(llave, 'g');
+            mensajeDesencriptado = mensajeDesencriptado.replace(expRegular, desencriptacion[llave]);
+        }
+
+        console.log(mensajeDesencriptado);
+    } else {
+        // Notificar
+        console.log("No es válido el texto digitado")
+
     }
-
-    console.log(mensajeDesencriptado);    
 }
