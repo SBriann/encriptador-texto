b4760af67abd977b2b16dfdb0f54033b8e839796
Brian <briann.sandovalm@gmail.com>
2024-07-25 23:40:48 -0500

    Implementación mostrar mensaje texto copiado

--- scripts/copiar.js
+++ scripts/copiar.js
@@ -5,8 +5,27 @@ function copiarTexto() {
 
         // Escribe el texto al portapapeles
         navigator.clipboard.writeText(resultado);
-        
-        console.log("Se ha copiado en el portapapeles");
+
+        // Muestra el mensaje de texto copiado
+        modificarClaseElemento(
+            [TEXTO_INVALIDO, RESULTADO, TEXTO_VACIO], // Elementos que se quieren ocultar
+            TEXTO_COPIADO // Elemento que se quiere mostrar
+        )
+
+        // Deshabilita los botones encriptar y desencriptar
+        document.getElementById("boton_encriptar").setAttribute("disabled", "true");
+        document.getElementById("boton_desencriptar").setAttribute("disabled", "true");
+
+        setTimeout(() => {
+            modificarClaseElemento(
+                [TEXTO_INVALIDO, RESULTADO, TEXTO_COPIADO], // Elementos que se quieren ocultar
+                TEXTO_VACIO // Elemento que se quiere mostrar
+            )
+            document.getElementById("boton_encriptar").removeAttribute("disabled")
+            document.getElementById("boton_desencriptar").removeAttribute("disabled");
+        }, 2000);
+
+
     } catch (error) {
         console.error('No se pudo copiar el texto: ', error);
     }
